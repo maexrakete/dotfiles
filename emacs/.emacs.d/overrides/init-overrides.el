cca72197b6c50f401cbde33200dd8fb7fde2e3f3
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2024-12-17 11:20:56 +0100

    switch to chezmoi

--- emacs/.emacs.d/overrides/init-overrides.el
+++
@@ -1,6 +0,0 @@
-(require 'init-theme)
-(require 'init-ace-window)
-(require 'init-eyebrowse)
-(require 'init-treemacs)
-
-(provide 'init-overrides)
--- emacs/.emacs.d/overrides/init-treemacs.el
+++
@@ -1,9 +0,0 @@
-(use-package treemacs
-  :ensure t
-  :init
-  :config
-  (global-set-key (kbd "M-<tab>") 'treemacs)
-)
-
-(provide 'init-treemacs)
-
